61b935a729b7198d6fe70e40afc5e0286dc2326e
Benjamin Hansen <4463738@RTS-365.dk>
2020-06-19 12:59:46 +0200

    add sticky header

--- src/javascript/sticky.js
+++ src/javascript/sticky.js
@@ -1,8 +1,11 @@
-function myFunction() {
-    var navbar = document.getElementById("mobile__nav");
-    if (navbar.className === "mobile__nav") {
-      navbar.className += " responsive";
-    } else {
-      navbar.className = "mobile__nav";
-    }
+var navbar = document.getElementById("header");
+var logo = document.getElementById("logo");
+document.onscroll = function () {
+  if (document.documentElement.scrollTop > 40) {
+    navbar.classList.add("scrolled");
+    logo.classList.add("scrolled-marked");
+  } else {
+    navbar.classList.remove("scrolled");
+    logo.classList.remove("scrolled-marked");
   }
+};
